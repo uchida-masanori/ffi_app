d44f0ee4d093c48cd7a9dc827e0a1183125de2ee
uchida-masanori <masa11soc@icloud.com>
2024-02-16 15:40:30 +0900

    dart:ffiでC++関数をdartで呼び出してios/androidで使用する

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'native_add.dart';
 import 'util/logger.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   // Flavor を取得し Logging
   const flavor = String.fromEnvironment('FLAVOR');
   logger.i('FLAVOR : $flavor');
@@ -23,53 +25,24 @@ class MyApp extends StatelessWidget {
       ),
       // アプリ内文字サイズを固定（本体設定の影響を受けない）
       builder: (context, child) => MediaQuery.withNoTextScaling(child: child!),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('1 + 2 == ${nativeAdd(1, 2)}'),
+          ),
+        ],
       ),
     );
   }
